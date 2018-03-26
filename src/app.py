6f9f353c51faa146dfa2519a32d2752f8e4c39c8
Kevin Wood <kevin.wood75@gmail.com>
2018-03-25 22:01:06 -0400

    Add more functions to blog app
    Signed-off-by: Kevin Wood <kevin.wood75@gmail.com>

--- src/app.py
+++ src/app.py
@@ -1,12 +1,75 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, session
+from src.models.user import User
+from src.models.blog import Blog
+from src.common.database import Database
 
 app = Flask(__name__)
+app.secret_key = "woodez"
 
 
 @app.route('/')
-def hello_method():
+def home_template():
+    return render_template('home.html')
+
+
+@app.route('/login')
+def login_template():
     return render_template('login.html')
 
+
+@app.route('/register')
+def register_template():
+    return render_template('resgister.html')
+
+
+@app.before_first_request
+def initialize_database():
+    Database.initialize()
+
+
+@app.route('/auth/login', methods=['POST'])
+def login_user():
+    email = request.form['email']
+    password = request.form['password']
+
+    if User.login_valid(email, password):
+        User.login(email)
+    else:
+        session['email'] = None
+
+    return render_template("profile.html", email=session['email'])
+
+
+@app.route('/auth/register', methods=['POST'])
+def register_user():
+    email = request.form['email']
+    password = request.form['password']
+
+    User.register(email, password)
+
+    return render_template("profile.html", email=session['email'])
+
+
+@app.route('/blogs/<string:user_id>')
+@app.route('/blogs')
+def user_blogs(user_id=None):
+    if user_id is not None:
+       user = User.get_by_id(user_id)
+    else:
+       user = User.get_by_email(session['email'])
+
+    blogs = user.get_blogs()
+
+    return render_template("user_blogs.html", blogs=blogs, email=user.email)
+
+
+@app.route('/posts/<string:blog_id>')
+def blog_posts(blog_id):
+    blog = Blog.from_mongo(blog_id)
+    posts = blog.get_posts()
+    
+    return render_template('posts.html', posts=posts, blog_title=blog.title)
+
 if __name__ == '__main__':
-    app.run(port=5000)
+    app.run(port=5000, debug=True)
 
